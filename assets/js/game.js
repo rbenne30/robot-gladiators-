ddb99b53eafd14d5bbf219c289651b452b540e40
rbenne30 <ryannebennett1@outlook.com>
2022-02-19 19:25:34 -0500

    update fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,10 +23,17 @@ var fight = function() {
    console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
    );
+   // check enemy's health
+if (enemyHealth <= 0) {
+   window.alert(enemyName + " has died!");
+ } 
+ else {
+   window.alert(enemyName + " still has " + enemyHealth + " health left.");
+ }
    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
  
    // Log a resulting message to the console so we know that it worked.
    console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
- };
+   };
